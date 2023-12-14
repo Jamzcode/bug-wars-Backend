f1e6bdcf736f7f4b45cf317080388e92f58e1b35
Reiczel Bael <reiczelbael.dev@gmail.com>
2023-12-14 07:44:53 -0800

    initial commit

---
+++ src/main/java/com/bugwars/App/model/User.java
@@ -0,0 +1,27 @@
+package com.bugwars.App.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
+
