8deaaa9a8943a52c3a3e452675efe32db1f37506
Steve Her <122183092+Sth84478@users.noreply.github.com>
2024-01-08 00:39:32 -0600

    added email column to user entity

--- src/main/java/com/bugwarsBackend/bugwars/model/User.java
+++ src/main/java/com/bugwarsBackend/bugwars/model/User.java
@@ -13,7 +13,6 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "users")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long user_id;
