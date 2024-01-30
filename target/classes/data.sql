f2147021519cbcf8e558764323b994a1bd2cb7b4
Steve Her <122183092+Sth84478@users.noreply.github.com>
2024-01-29 21:52:38 -0600

    updated data.sql

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -18,6 +18,6 @@ VALUES
     '[30, 11, 32, 14, 34, 17, 31, 17, 10, 35, 0, 13, 35, 0, 14, 35, 0, 11, 35, 0]', true),
     ((SELECT id FROM users WHERE username = 'spider_bug'), 'Venom Strike', 'Script 2',
     '[33, 5, 0, 35, 0, 12, 35, 0]', true),
-     ((SELECT id FROM users WHERE username = 'spider_bug'), 'Venom Strike', 'Script 3',
-     '[33, 5, 0, 35, 0, 12, 35, 0]', true);
+     ((SELECT id FROM users WHERE username = 'spider_bug'), 'Spider Strike', 'Script 3',
+     '[13, 13, 32, 9, 10, 11, 12, 35, 9, 14, 11, 11, 35, 0]', true);
 
